0e5a07ee354bcbd47e79225b5850312b7a868851
WongueShin <won7963kr@gmail.com>
2022-01-14 00:48:25 +0900

    declare Main Bar tags

--- src/components/MainBar.js
+++ src/components/MainBar.js
@@ -1,17 +1,21 @@
 import './MainBar.css'
 function MainBar() {
     return(
-        <nav>
-            <div role='presentation' className="MainBar_className">
-                <button type="button" className="MainBar_hamberger">
-                    <img src="https://static.wanted.co.kr/images/icon-menu.png"
-                        style={{width:"17px", heigth:"14px", objectFit:"contain"}}/>
-                </button>
-                <a>
-                    <i className='icon_new'></i>
-                </a>
-            </div>
-        </nav>
+        <div role='presentation' className="MainBar_className">
+            <nav className='MainBar_nav'>
+                <div className='MainBar_nav_top'>
+                    <button type="button" className="MainBar_hamberger">
+                        <img src="https://static.wanted.co.kr/images/icon-menu.png"
+                            style={{width:"17px", heigth:"14px", objectFit:"contain"}}/>
+                    </button>
+                    <a>
+                        <i className='icon_new'></i>
+                    </a>
+                </div>
+                <ul className='MainBar_Menu'></ul>
+                <aside className='Aside_className'></aside>
+            </nav>
+        </div>
     )
 }
 
