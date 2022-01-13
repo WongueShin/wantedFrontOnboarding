177781bfb724e1a8f2e3303355aef63423f35222
WongueShin <won7963kr@gmail.com>
2022-01-14 00:38:46 +0900

    import Logo font-family

--- src/components/MainBar.js
+++ src/components/MainBar.js
@@ -1,8 +1,17 @@
+import './MainBar.css'
 function MainBar() {
     return(
-        <div role='presentation' className="MainBar_className">
-            <h1>MainBar</h1>
-        </div>
+        <nav>
+            <div role='presentation' className="MainBar_className">
+                <button type="button" className="MainBar_hamberger">
+                    <img src="https://static.wanted.co.kr/images/icon-menu.png"
+                        style={{width:"17px", heigth:"14px", objectFit:"contain"}}/>
+                </button>
+                <a>
+                    <i className='icon_new'></i>
+                </a>
+            </div>
+        </nav>
     )
 }
 
