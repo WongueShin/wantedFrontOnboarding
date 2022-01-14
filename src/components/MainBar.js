97ac2a1cc9dbe88250afc9a5a57e6d08945565cb
WongueShin <won7963kr@gmail.com>
2022-01-14 17:14:28 +0900

    add img, font file to local. make some Adaptive design tags

--- src/components/MainBar.js
+++ src/components/MainBar.js
@@ -2,19 +2,24 @@ import './MainBar.css'
 function MainBar() {
     return(
         <div role='presentation' className="MainBar_className">
-            <nav className='MainBar_nav'>
-                <div className='MainBar_nav_top'>
-                    <button type="button" className="MainBar_hamberger">
-                        <img src="https://static.wanted.co.kr/images/icon-menu.png"
-                            style={{width:"17px", heigth:"14px", objectFit:"contain"}}/>
-                    </button>
-                    <a>
-                        <i className='icon_new'></i>
-                    </a>
-                </div>
-                <ul className='MainBar_Menu'></ul>
-                <aside className='Aside_className'></aside>
-            </nav>
+            <div className='MainBar_margin'>
+                <nav className='MainBar_nav'>
+                    <div className='MainBar_nav_top'>
+                        <div className='MainBar_nav_topLogo'>
+                            <button type="button" className="MainBar_hamberger">
+                                <img src="./images/icon-menu.png"
+                                    style={{width:"17px", heigth:"14px", objectFit:"contain"}}/>
+                            </button>
+                            <a>
+                                <i className='icon_new'></i>
+                            </a>
+                        </div>
+                        <button className='SignUpButton'>회원가입하기</button>
+                    </div>
+                    <ul className='MainBar_Menu'></ul>
+                    <aside className='Aside_className'></aside>
+                </nav>
+            </div>
         </div>
     )
 }
